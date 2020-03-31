63ce40fddae8260c580ad798c96fd8922325dca0
Julien DAURAT <juju786601@yahoo.fr>
2020-03-31 20:17:39 +0200

    redirection fixes - add user to DB by username - forgetPassword Working - play route code clean

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ app.use(bodyP.json());
 app.use(bodyP.urlencoded({ extended: true }));
 const port = 3000;
 
+// ADD STATIC FOLDERS AND FILES USED IN THE PROGRAM
 app.use(express.static("views"));
 app.use(express.static("views/Home"));
 app.use(express.static("views/Login"));
 app.use(express.static("views/Register"));
+app.use(express.static("views/Play"));
+app.use(express.static("firebase.js"));
 
 // ENGINE USE TO RENDER
 var nunjucks = require('nunjucks');
@@ -18,8 +21,6 @@ nunjucks.configure('views', {
     autoescape: true
 });
 app.set('view engine', 'html');
-
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
@@ -32,10 +33,10 @@ require('firebase/database');
 var firebaseConfig = require('./firebase.js');
 firebase.initializeApp(firebaseConfig.getFirebaseConfig());
 
-
+/****** Routes *******/
 app.get('/', function(req, res){
-    console.log("Utilisateur: " + firebase.auth().currentUser);
-    if(!firebase.auth().currentUser){
+    var user = firebase.auth().currentUser;
+    if(!user){
         res.sendFile('home.html', { root: __dirname + "/views/Home" } );
     }
     else{
@@ -43,7 +44,7 @@ app.get('/', function(req, res){
     }
 });
 
-/****** Routes *******/
+/****** REGISTER *******/
 app.get('/register', function(req, res) {
     if(!firebase.auth().currentUser){
         res.render('Register/register');
@@ -54,7 +55,7 @@ app.get('/register', function(req, res) {
 });
 
 app.post('/register', async function(req, res) {
-    var x = new Boolean("false");
+    var firstStateChange = true;
     var db = firebase.firestore();
 
     var username = req.body.username;
@@ -63,76 +64,89 @@ app.post('/register', async function(req, res) {
     var repassword = req.body.re_password;
 
     // DECONNECTE L'UTILISATEUR POSSIBLEMENT DEJA CONNECTE
-    firebase.auth().signOut();
+    if(firebase.auth().currentUser) firebase.auth().signOut();
+
 
     if(password == repassword)
     {
-
-        // AJOUT COMPTE AUTHENTIFICATION
-        firebase.auth().createUserWithEmailAndPassword(email,password).catch(function(error)
-        {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(errorCode + ":" + errorMessage);
-
-            res.render('Register/register', {error_message: errorMessage,
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                re_password: req.body.re_password
-            });
-        });
-        console.log("Account email:" + email + " successfully created (Username:" + username + ")");
-
-        // AJOUT UTILISATEUR BD
-        firebase.auth().onAuthStateChanged(function(user)
-        {
-            if(user){
-                console.log("Utilisateur co: " + user.uid);
-
-                // ADD USERNAME INFO TO AUTH SYSTEM
-                user.updateProfile({
-                    displayName: username
-                }).then(function() {
-                    console.log("Username set to:" + username);
-
-                    // ADD USER TO THE DB
-                    var userUID = user.uid;
-                    let data = {
-                        username : username,
+        //CHECK IF USER ALREADY EXISTS IN DB
+        const usersRef = db.collection('users').doc(username);
+        usersRef.get()
+            .then((docSnapshot) => {
+                if (docSnapshot.exists) {   // IF USERNAME ALREADY EXISTS -> STOP PROCESS
+                    console.log("User " + username + " already exists in DB");
+                    res.render('Register/register', {error_message: "Username is already in use !",
+                        username: username,
                         email: email,
-                        win: 0,
-                        lost: 0
-                    };
-                    db.collection("users").doc(userUID).set(data)
-                        .then(function() {
-                            console.log("User {Username:" + user.displayName + " - UID:" + userUID + " - email:" + user.email + "} has been added to the DB");
-                            res.render('main', { username: user.displayName, uid: user.uid });
-                            return;
-                        })
-                        // IF USER CAN'T BE ADDED TO DB, REMOVING ACCOUNT FROM FIREBASE AUTHENTICATION
-                        .catch(function(error) {
-                            console.error("Error adding user: ", error);
-                            var user = firebase.auth().currentUser;
-                            if(user){
-                                console.error("USER LOGGED");
-                                user.delete().then(function() {
-                                    console.error("USER ACCOUNT DELETED");
+                        password: password,
+                        re_password: repassword
+                    });
+                } else {    // IF USER DOESN'T EXISTS -> CONTINUE CREATION PROCESS
+                    // AJOUT COMPTE AUTHENTIFICATION
+                    firebase.auth().createUserWithEmailAndPassword(email,password).then(function(firebaseUser) {
+                        console.log("Account {Email:" + email + " - username:" + username + "} successfully created");
+                        // AJOUT UTILISATEUR BD
+                        firebase.auth().onAuthStateChanged(function(user)
+                        {
+                            if(user && firstStateChange){
+                                firstStateChange = false;
+                                // ADD USERNAME INFO TO AUTH SYSTEM
+                                user.updateProfile({
+                                    displayName: username
+                                }).then(function() {
+                                    // ADD USER TO THE DB
+                                    var userUID = user.uid;
+                                    let data = {
+                                        username : username,
+                                        UID : userUID,
+                                        email: email,
+                                        win: 0,
+                                        lost: 0
+                                    };
+                                    db.collection("users").doc(username).set(data)
+                                        .then(function() {
+                                            console.log("User {Username:" + username + " - UID:" + userUID + " - email:" + email + "} added to DB");
+                                            res.redirect('/main');
+                                        })
+                                        // IF USER CAN'T BE ADDED TO DB, REMOVING ACCOUNT FROM FIREBASE AUTHENTICATION
+                                        .catch(function(error) {
+                                            console.error("Error adding user: ", error);
+                                            var user = firebase.auth().currentUser;
+                                            if(user){
+                                                console.error("USER LOGGED");
+                                                user.delete().then(function() {
+                                                    console.error("USER ACCOUNT DELETED");
+                                                }).catch(function(error) {
+                                                    console.error("Error deleting user account: ", error);
+                                                });
+                                            }
+                                            console.log("REDIRECTION VERS /register a faire");
+                                        });
+
                                 }).catch(function(error) {
-                                    console.error("Error deleting user account: ", error);
+                                    console.error("Error setting username: ", error);
                                 });
+
                             }
-                            console.log("REDIRECTION VERS /register a faire");
                         });
-                }).catch(function(error) {
-                    console.error("Error setting username: ", error);
-                });
+                    })
+                        .catch(function(error)
+                        {
+                            var errorCode = error.code;
+                            var errorMessage = error.message;
+                            console.log(errorCode + ":" + errorMessage);
 
-            }
-        });
-
+                            res.render('Register/register', {error_message: errorMessage,
+                                username: username,
+                                email: email,
+                                password: password,
+                                re_password: repassword
+                            });
+                        });
+                }
+            });
     }
-    else
+    else        // IF PASSWORD AND PASSWORD CONFIRMATION ARE NOT ==
     {
         res.render('Register/register', {error_message: 'Passwords do not match !',
             username: req.body.username,
@@ -143,6 +157,7 @@ app.post('/register', async function(req, res) {
     }
 });
 
+/****** LOGIN *******/
 app.get('/login', function(req, res) {
     var user = firebase.auth().currentUser;
     if(!user){
@@ -154,57 +169,97 @@ app.get('/login', function(req, res) {
 });
 
 app.post('/login', async function(req, res) {
-    var x = true;
-
-    console.log("/login post");
+    var firstStateChange = true;
     try {
         var email = req.body.email;
         var password = req.body.password;
 
         firebase.auth().signInWithEmailAndPassword(email, password).then(function(firebaseUser) {
-            //res.redirect('/main');
             firebase.auth().onAuthStateChanged(function(user) {
-                if (user && x) {
-                    x = false;
+                if (user && firstStateChange) {
+                    firstStateChange = false;
                     console.log("username:" + user.displayName + "-" + "UID:" + user.uid);
-                    res.render('main', { username: user.displayName, uid: user.uid });
-                    //res.redirect("/main");
-                    /*res.end();
-                    return;*/
+                    res.redirect("/main");
                 }
             });
         })
-        .catch(function(error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(errorCode + ":" + errorMessage);
+            .catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode + ":" + errorMessage);
 
-            res.render('Login/login', {error_message: errorMessage,
-                email: req.body.email,
-                password: req.body.password});
-        });
+                res.render('Login/login', {error_message: errorMessage,
+                    email: req.body.email,
+                    password: req.body.password});
+            });
     }
     catch(error) {
         console.error("Login error:" + error);
     }
 });
 
+/****** FORGET PASSWORD *******/
 app.get('/forgetPassword', function(req, res) {
-  res.sendFile('forgetPassword.html', { root: __dirname + "/views/Login" } );
+    var user = firebase.auth().currentUser;
+    if(!user){
+        res.render('Login/forgetPassword');
+    }
+    else{
+        res.redirect("/main");
+    }
 });
 
+app.post('/forgetPassword', async function(req, res) {
+    try {
+        var auth = firebase.auth();
+        var email = req.body.email;
+
+        auth.sendPasswordResetEmail(email).then(function()
+        {
+            res.render('Login/forgetPassword', {result_message: "The instructions to reset your password has been sent to you"});
+        })
+            .catch(function(error)
+            {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                console.log(errorCode + ":" + errorMessage);
+                res.render('Login/forgetPassword', {result_message: errorMessage});
+            });
+    }
+    catch(error) {
+        console.error("Reset password error:" + error);
+    }
+});
+
+
+
+
+/****** MAIN *******/
 app.get('/main', function(req, res) {
-    if(!firebase.auth().currentUser){
+    var user = firebase.auth().currentUser;
+    if(!user){
         res.redirect('/login');
     }
     else{
-        res.sendFile('main.html', { root: __dirname + "/views" } );
+        res.render('main', { username: user.displayName, uid: user.uid });
     }
 });
 
+/****** DISCONNECT *******/
 app.get('/disconnect', function(req, res) {
     if(firebase.auth().currentUser){
         firebase.auth().signOut();
     }
     res.redirect('/');
+});
+
+/****** PLAY *******/
+app.get('/play', function(req, res) {
+    if(firebase.auth().currentUser){
+        res.sendFile('play.html', { root: __dirname + "/views/Play" } );
+    }
+    else{
+        res.redirect('/');
+    }
 });
